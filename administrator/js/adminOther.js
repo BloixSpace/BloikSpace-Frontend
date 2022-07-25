cf547ac65e52f5e17e68462afae8a27dfcbd4595
yueyangw <yueyang_wu@outlook.com>
2022-07-25 12:43:29 +0800

    1

--- administrator/js/adminOther.js
+++ administrator/js/adminOther.js
@@ -3,4 +3,7 @@
     if (local.getItem("level") != 3) {
         document.getElementById("manage1").style.display = "none";
     }
+    if (local.getItem("ifLogin") == 1) {
+        document.getElementById("login").style.display = "none";
+    }
 }
